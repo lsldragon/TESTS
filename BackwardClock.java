cc1edb7946d53a59ba54ddfd1037cab0b775a056
Elliot 李 <31510795+lsldragon@users.noreply.github.com>
2019-04-25 21:45:34 +0800

    Update BackwardClock.java

--- BackwardClock.java
+++ BackwardClock.java
@@ -16,10 +16,6 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.FileInputStream;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Properties;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
